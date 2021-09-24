6502f40083ea363dc62841b38fb6bcbb5b317b66
Chinmay <chinmaytechnicals@gmail.com>
2021-09-24 13:38:45 +0530

    Pizza models created

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    sno = models.AutoField(primary_key=True)
+    Pizza_name = models.CharField(max_length=200)
+    Pizza_desc = models.CharField(max_length=400)
+    Pizza_price = models.FloatField()
+    image_url = models.TextField(default="")
+    def __str__(self):
+        return self.Pizza_name
+    
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .models import Pizza
 # Create your views here.
 def home(request):
-    return render(request, "Home/index.html")
+    pizza = Pizza.objects.all()
+    last = pizza[len(pizza)-1]
+    second_last = pizza[len(pizza)-2]
+    third_last = pizza[len(pizza)-3]
+    forth_last = pizza[len(pizza)-4]
+    context = {
+        'pizza': pizza,
+        'last': last,
+        'second_last': second_last,
+        'third_last':third_last,
+        'forth_last':forth_last,
+    }
+    return render(request, "Home/index.html", context)
